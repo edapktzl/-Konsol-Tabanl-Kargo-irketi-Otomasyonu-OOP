30cd2e35b20c1fe0c14b8c1bfdca1cbf5f451269
Edanur <pktzleda@gmail.com>
2026-04-29 16:55:44 +0300

    users.cs dosyası eklendi

--- KonsolTabanlıKargoŞirketiOtomasyonu/users.cs
+++ KonsolTabanlıKargoŞirketiOtomasyonu/users.cs
@@ -11,5 +11,6 @@ namespace KonsolTabanlıKargoŞirketiOtomasyonu
         public string Surname { get; set; }
         public string Password { get; set; }
         public string Position { get; set; }
+        
     }
 }
